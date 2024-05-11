df4cc4baf6c93e84c91790f31a53539e12c8943e
Jahanzeb Khan <jahanzebkhan@Jahanzebs-MacBook-Air.local>
2024-05-11 14:13:03 +0100

    PWA config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,65 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import BottomNav from "./ui/nav/bottom-nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "GymPal";
+const APP_DEFAULT_TITLE = "GymPal";
+const APP_TITLE_TEMPLATE = "%s - GymPal";
+const APP_DESCRIPTION = "Record your workouts";
+
 export const metadata: Metadata = {
-title: "GymPal",
-description: "Record your workouts",
-generator: "Next.js",
-manifest: "/manifest.json",
-keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
-themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
-children,
-}: Readonly<{ children: React.ReactNode; }>) {
-    return (
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
     <html lang="en">
-
-    <body className={inter.className}>
+      <body className={inter.className}>
         <div className="text-2xl p-4 border-b">GymPal</div>
         {children}
         <BottomNav />
-    </body>
-
+      </body>
     </html>
-    );
-    }
+  );
+}
